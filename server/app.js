88b266a331a8f54a377251a2f01d8ac2139e4c6e
mdibe <m.dibe@magnetomedia.net>
2023-09-02 21:22:51 +0100

    Add: database connection

--- server/app.js
+++ server/app.js
@@ -2,6 +2,64 @@ const express = require('express');
 const { graphqlHTTP }  = require('express-graphql');
 const schema = require('./schema/schema')
 const app = express();
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://meryem:meryem@gql-cluster.xzewhai.mongodb.net/gql?retryWrites=true&w=majority";
+
+// -------- database connection --------
+
+// another way of connecting to database:
+mongoose.connect(uri);
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'cnx error'));
+db.once('open',() => {
+    console.log("connected successfuly");
+});
+
+
+// -------- database connection --------
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("gql").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+
+
+// run().catch(console.dir);
+
+
+// -------- database connection --------
+
+// ---- test database connection 
+// app.get('/test-mongodb-connection', async (req, res) => {
+//     try {
+//       // Attempt to ping the database to check the connection
+//       await client.db("admin").command({ ping: 1 });
+//       res.status(200).json({ message: "MongoDB connection test successful!" });
+//     } catch (error) {
+//       console.error("MongoDB connection test failed:", error);
+//       res.status(500).json({ error: "MongoDB connection test failed" });
+//     }
+//   });
 
 app.use('/graphql',graphqlHTTP({
     schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,21 +2,8 @@ const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 const _ = require('lodash');
 
-// dummy data
-var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId:'1' },
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId:'2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId:'3' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
-    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
-    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
-];
-
-var authors = [
-    { name: 'Patrick Rothfuss', age: 44, id: '1' },
-    { name: 'Brandon Sanderson', age: 42, id: '2' },
-    { name: 'Terry Pratchett', age: 66, id: '3' }
-];
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 // 1 - create our object types : books & authors
  const BookType = new GraphQLObjectType({
@@ -28,7 +15,7 @@ var authors = [
         author: {
             type : Authortype,
             resolve(parent, args) {
-                return _.find(authors, {id: parent.authorId} )
+                // return _.find(authors, {id: parent.authorId} )
             }
         }
 
@@ -45,11 +32,47 @@ var authors = [
         books: {
             type: new GraphQLList(BookType),
             resolve(parent,args) {
-                return _.filter(books, {authorId :parent.id })
+                // return _.filter(books, {authorId :parent.id })
             }
         }
 
     })
+
+ });
+
+ const Mutation = new GraphQLObjectType({
+    name:'Mutation',
+    fields:{
+        addAuthor: {
+            type: Authortype,
+            args: {
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt},
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age,
+                })
+               return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                })
+               return book.save();
+            }
+        }         
+    }
 
  });
 
@@ -63,26 +86,26 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLID}}, // book(id:123){name genre...}
             resolve(parent, args) {
                 // code to get data from db / other source
-                 return _.find(books, {id: args.id});
+                //  return _.find(books, {id: args.id});
             }
         },
         author: {
             type:Authortype,
             args: {id: {type: GraphQLID}}, 
             resolve(parent, args) {
-                 return _.find(authors, {id: args.id});
+                //  return _.find(authors, {id: args.id});
             }
         },
         books: {
             type: new GraphQLList(BookType),
              resolve(parent, args) {
-                 return books;
+                //  return books;
             }
         }, 
         authors: {
             type: new GraphQLList(Authortype),
              resolve(parent, args) {
-                 return authors;
+                //  return authors;
             }
         }, 
     }
@@ -90,7 +113,8 @@ const RootQuery = new GraphQLObjectType({
  });
 // export schema to use it in app.js
 module.exports = new GraphQLSchema({
-    query : RootQuery
+    query : RootQuery,
+    mutation: Mutation
 })
 
 // Wrapping fields inside functions aims to make those fields inside a function 
