fc6a201265539ef8fa69e30153cf92d65f415ada
mdibe <m.dibe@magnetomedia.net>
2023-09-02 22:50:13 +0100

    Add: react app

---
+++ client/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BookList() {
+    return (
+      <div id="main">
+        <ul>
+            <li>Book 1</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default BookList;
+  
